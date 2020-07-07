48634c425c8e22c5928dd08c0363ca8f8d43fdf2
SaxenaKartik <kartiksaxena500@gmail.com>
2020-07-07 07:44:07 +0530

    Added environments file

---
+++ config/environments.js
@@ -0,0 +1,8 @@
+import {Dimensions} from 'react-native';
+
+export const window = Dimensions.get("window");
+export const deviceHeight = window.height;
+export const deviceWidth = window.width;
+export const primaryColor = "#ee4e34";
+export const secondaryColor = "#d4d4d4";
+export const commonBackgroundColor = "#fcedda";
