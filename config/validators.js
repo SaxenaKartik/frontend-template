c710839731a093ecb0a578e45c27fcd74d499691
SaxenaKartik <kartiksaxena500@gmail.com>
2020-07-12 04:37:07 +0530

    Updated password validator and added items & address validator

--- config/validators.js
+++ config/validators.js
@@ -3,7 +3,7 @@ export const validateMobile = (number) => {
 }
 
 export const validatePassword = (password) => {
-  return /(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/.test(password)? true:false;
+  return /(?=.*[0-9])(?=.*[!@#\$%\^&\*\-\(\)\+\=\~\`\\\/\?\[\]\{\}\.\<\>\"\'\;\:\|])(?=.{8,})/.test(password)? true:false;
 }
 
 export const validateName = (name) =>{
@@ -12,4 +12,20 @@ export const validateName = (name) =>{
 
 export const validateEmail = (email) => {
   return /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test(email) ? true : false;
+}
+
+export const validateItems = (items) => {
+  var list = items.split('\n')
+  let re_single = /^([a-zA-Z])+(\s|:| : |: | :)[0-9]+[a-zA-Z]*$/
+  match = list.every((value)=>{return re_single.test(value);})
+  // console.log(match)
+  let re = /[a-zA-Z]+(\s|:| : |: | :)[0-9]+[a-zA-Z]*/gm
+  var count = ((items || '').match(re) || []).length
+  let re_count_lines = /\n/gm
+  var count_lines = ((items || '').match(re_count_lines) || []).length
+  return count_lines+1==count && match ? true : false
+}
+
+export const validateAddr = (addr) => {
+  return /^null|$/.test(addr) ? true : false;
 }
